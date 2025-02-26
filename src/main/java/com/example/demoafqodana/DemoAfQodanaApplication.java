f3d68359a6dcc92d1e498de453f0249831904d9c
Nilket <joni0010@stud.kea.dk>
2025-02-26 10:39:11 +0100

    Initial commit

---
+++ src/main/java/com/example/demoafqodana/DemoAfQodanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoafqodana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAfQodanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAfQodanaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoafqodana/DemoAfQodanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoafqodana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAfQodanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
